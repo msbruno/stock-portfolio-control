d544aad922818b331287b1c95df0a3aa4f6f8d96
msbruno <ms.bruno1@gmail.com>
2021-09-18 17:39:57 -0300

    refacor: move column mapper to use-cases/interfaces

---
+++ src/use_cases/interfaces/mappers.py
@@ -0,0 +1,26 @@
+from src.use_cases.process_operations.portfolio_manager import OperationType
+
+
+class ColumnMapper:
+
+    def __init__(self, data:str, ticker:str, operation:str, quantity:str, mean_price:str) -> None:
+        self.__data = data
+        self.__ticker = ticker
+        self.__operation = operation
+        self.__quantity = quantity
+        self.__mean_price = mean_price
+
+    def data_column(self):
+        return self.__data
+
+    def ticker_column(self):
+        return self.__ticker
+
+    def operation_column(self):
+        return self.__operation
+
+    def quantity_column(self):
+        return self.__quantity
+
+    def mean_price_column(self):
+        return self.__mean_price
